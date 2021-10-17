5c53bac4fc0d89cc8d7d4df0449ed362ebb59f62
Julian Alvarez <alvarez.julian@correounivalle.edu.co>
2021-10-17 02:19:41 -0500

    Reconocimiento de monedas aun no funciona

--- Curso Reconocimiento Facial/Reconocimiento/MonedasContorno/ContarMonedasCamara.py
+++ Curso Reconocimiento Facial/Reconocimiento/MonedasContorno/ContarMonedasCamara.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def ordernarPuntos(puntos):
-    nPuntos = np.concatenate(puntos[0],puntos[1],puntos[2],puntos[2]).tolist()
+    nPuntos = np.concatenate([puntos[0],puntos[1],puntos[2],puntos[2]]).tolist()
     y = sorted(nPuntos,key=lambda nPuntos:nPuntos[1])
     x1 = y[0:2]
     x1 = sorted(x1,key=lambda x1: x1[0])
@@ -15,9 +15,9 @@ def alineamiento(imagen,ancho,alto):
     grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
     tipoUmbral,umbral = cv2.threshold(grises,150,255,cv2.THRESH_BINARY)
     cv2.imshow("Umbral",umbral) 
-    contorno, jerarquia = cv2.findContours(umbral,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+    contorno = cv2.findContours(umbral,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 
-    contorno = sorted(contornos,key=cv2.contourArea(), reverse=True)[:1]
+    contorno = sorted(contorno,key=cv2.contourArea, reverse=True)[:1]
 
     for c in contorno:
         epsilon = 0.01*cv2.arcLength(c,True)
@@ -30,3 +30,59 @@ def alineamiento(imagen,ancho,alto):
 
             M = cv2.getPerspectiveTransform(puntos1,puntos2)
             imagenAlineada = cv2.warpPerspective(imagen,M, (ancho,alto) )
+    return imagenAlineada
+
+capturaVideo = cv2.VideoCapture(0)
+
+while True:
+    tipoCamara,camara = capturaVideo.read()
+
+    if tipoCamara == False:
+        break
+    imagen_A6 = alineamiento(camara,ancho = 480, alto = 677)
+    if imagen_A6 is not None:
+        puntos = []
+        imagenGris = cv2.cvtColor(imagen_A6,cv2.COLOR_BGR2GRAY)
+        gauss = cv2.GaussianBlur(imagenGris,(5,5),1)
+        _,Umbral2 = cv2.threshold(gauss,0,255,cv2.THRESH_OTSU+cv2.THRESH_BINARY_INV)
+        cv2.imshow("Umbral",Umbral2)
+        contorno2 = cv2.findContours(Umbral2,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
+        cv2.drawContours(imagen_A6,contorno2,-1,(0,255,0),2)
+
+        suma1 = 0.0
+        suma2 = 0.0
+
+        for c2 in contorno2:
+            area = cv2.contourArea(c2)
+            momentos = cv2.moments(c2)
+
+            if (momentos["m00"])==0:
+                momentos["m00"]=1.0
+            
+            x = int(momentos["m10"]/momentos["m00"])
+            y = int(momentos["m01"]/momentos["m00"])
+
+
+            if area < 9300 and area>8000:
+                font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+                cv2.putText(imagen_A6, "S/ 0.20",(x,y),font,0.75,(255,0,0),2)
+                suma1 += 0.2 
+
+            if area < 7800 and area>6500:
+                font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+                cv2.putText(imagen_A6, "S/ 0.10",(x,y),font,0.75,(255,0,0),2)
+                suma2 += 0.2 
+
+        total = suma1 + suma2
+
+        print(f"Sumatoria total en centimos: {round(total,2)} ")
+
+        cv2.imshow("Imagen A6", imagen_A6)
+        cv2.imshow("Camara",camara)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+capturaVideo.release()
+cv2.destroyAllWindows()
+
