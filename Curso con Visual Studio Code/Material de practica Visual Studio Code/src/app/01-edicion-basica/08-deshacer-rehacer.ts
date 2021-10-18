1006ca5b4500853754be54e3c3c18df1488a5743
Julian Alvarez <alvarez.julian@correounivalle.edu.co>
2021-10-17 19:34:55 -0500

    Realizando un curso de Visual Studio Code, mostrando los atajos y los ejercicios

---
+++ Curso con Visual Studio Code/Material de practica Visual Studio Code/src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,19 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
