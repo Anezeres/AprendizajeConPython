150ff6735f36158943f01135c4388545ea73e00a
Julian Alexander Alvarez Payares <alvarez.julian@correounivalle.edu.co>
2021-10-25 02:00:35 -0500

    Empezando a ver la libreria de math en Python

---
+++ RStudio y Python/Introduccion R/09-Taller con R.R
@@ -0,0 +1,36 @@
+250000000/60 = 4166667 #minutos.
+
+4166667%/%60 = 69444 #horas y
+4166667%%60 = 27 #minutos
+
+69444%/%24 = 2893 #días y 
+69444%%24 = 12 #horas
+
+2893%/%365 = 7 #años (de los cuales 2 son bisiestos) y 
+(2893%%365)-2 = 336 #días
+
+#RESPUESTA: Estaríamos en el día 336 del año 2025 
+#(lo que equivale al 2 de diciembre de 2025) y serían exactamente las 12:27 a.m
+
+#Segundo Ejercicio
+
+primerGrado = function(A,B,C){
+  #Ax+B=C
+  x = (C-B)/A;
+  x
+  
+}
+
+primerGrado(2,4,0)
+primerGrado(5,3,0)
+primerGrado(7,4,18)
+primerGrado(1,1,1)
+
+#Tercer Ejercicio 
+round(3*exp(1)-pi,3)
+
+5.013
+
+round(Mod((2+3i)^2/(5+8i)),3)
+
+1.378
